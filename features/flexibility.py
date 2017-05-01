a61b527fa40dad0cb78ff2b48b646cae302f1df1
emsuter715 <emsuter715@gmail.com>
2017-05-01 12:37:06 -0700

    Update flexibility.py

--- features/flexibility.py
+++ features/flexibility.py
@@ -10,10 +10,11 @@ import Bio.PDB
 
 def feature(chain):
     
-    flexibility={"Ala":  0.360, "Arg":  0.530, "Asn":  0.460, "Asp":  0.510, "Cys":  0.350,
-              "Gln":  0.490, "Glu":  0.500, "Gly":  0.540, "His":  0.320, "Ile":  0.460,
-              "Leu":  0.370, "Lys":  0.470, "Met":  0.300, "Phe":  0.310, "Pro":  0.510,
-              "Ser":  0.510, "Thr":  0.440, "Trp":  0.310, "Tyr":  0.420, "Val":  0.390}    
+    flexibility={"Ala": 0.360, "Arg": 0.530, "Asn":  0.460, "Asp":  0.510, "Cys":  0.350,
+                 "Gln": 0.490, "Glu": 0.500, "Gly":  0.540, "His":  0.320, "Ile":  0.460,
+                 "Leu": 0.370, "Lys": 0.470, "Met":  0.300, "Phe":  0.310, "Pro":  0.510,
+                 "Ser": 0.510, "Thr": 0.440, "Trp":  0.310, "Tyr":  0.420, "Val":  0.390,
+                 "Sec": 0.350, "Pyl": 0.470}    
     
     #initialize dict of dict
     samples=dict()
