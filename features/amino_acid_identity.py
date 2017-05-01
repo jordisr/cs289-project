81ee7b379a529860b34cfa4136202ad62f0da77a
emsuter715 <emsuter715@gmail.com>
2017-05-01 12:32:30 -0700

    Update amino_acid_identity.py

--- features/amino_acid_identity.py
+++ features/amino_acid_identity.py
@@ -19,10 +19,11 @@ def feature(chain):
         
         #print(index)
         samples[index]={"isAla":0, "isArg":0, "isAsn":0, "isAsp":0, 
-                    "isCys":0, "isGln":0, "isGlu":0, "isGly":0,
-                    "isHis":0, "isIle":0, "isLeu":0, "isLys":0, 
-                    "isMet":0, "isPhe":0, "isPro":0, "isSer":0, 
-                    "isThr":0, "isTrp":0, "isTyr":0, "isVal":0}
+                        "isCys":0, "isGln":0, "isGlu":0, "isGly":0,
+                        "isHis":0, "isIle":0, "isLeu":0, "isLys":0, 
+                        "isMet":0, "isPhe":0, "isPro":0, "isSer":0, 
+                        "isThr":0, "isTrp":0, "isTyr":0, "isVal":0,
+                        "isSec":0, "isPyl":0}
     
         amino_acid=residue.get_resname().title()
         samples[index]["is"+str(amino_acid)]=1
@@ -33,5 +34,6 @@ def feature_names():
     return ["isAla", "isArg", "isAsn", "isAsp", "isCys", 
             "isGln", "isGlu", "isGly", "isHis", "isIle", 
             "isLeu", "isLys", "isMet", "isPhe", "isPro", 
-            "isSer", "isThr", "isTrp", "isTyr", "isVal"]
+            "isSer", "isThr", "isTrp", "isTyr", "isVal",
+            "isSec", "isPyl"]
     
