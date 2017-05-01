d417c2c2db072ce315ea85912f50b691f55bc40d
emsuter715 <emsuter715@gmail.com>
2017-05-01 12:41:30 -0700

    Update res_weight.py

--- features/res_weight.py
+++ features/res_weight.py
@@ -13,7 +13,8 @@ def feature(chain):
     molec_weight={"Ala": 89, "Arg": 174, "Asn": 132, "Asp": 133, "Cys": 121,
                   "Gln": 146, "Glu": 147, "Gly": 75, "His": 155, "Ile": 131, 
                   "Leu": 131, "Lys": 146, "Met": 149, "Phe": 165, "Pro": 115, 
-                  "Ser": 105, "Thr": 119, "Trp": 204, "Tyr": 181, "Val": 117}    
+                  "Ser": 105, "Thr": 119, "Trp": 204, "Tyr": 181, "Val": 117,
+                  "Sec": 167, "Pyl": 255}    
     
     #initialize dict of dict
     samples=dict()
